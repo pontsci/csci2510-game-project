becb5cac0240803d4e7f7d1cb477aec39942c461
pontsci <mgfleming@unomaha.edu>
2018-04-06 12:27:14 -0500

    wall moved, path change

--- src/steamHunt/WallManager.java
+++ src/steamHunt/WallManager.java
@@ -10,7 +10,7 @@ public class WallManager extends Manager{
     //Get the wall's sprite sheet and make two walls on either side of the screen
     public void initialize(){
         ArrayList<BufferedImage> spriteAnimations = new ArrayList<>();
-        spriteAnimations.add(loadFile("wall.png"));
+        spriteAnimations.add(loadFile("src/resources/foreground/wall/wall.png"));
         getSprites().add(new Wall(-7.9f, 1, new Vector2f(14,14), spriteAnimations));
         getSprites().add(new Wall(7.9f, 1, new Vector2f(-14,14), spriteAnimations));
     }
