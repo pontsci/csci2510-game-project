583083915eff3e9dfb50c10ca6e24fd25dfe66f9
pontsci <mgfleming@unomaha.edu>
2018-04-06 12:22:42 -0500

    floor.png move, path change

--- src/steamHunt/FloorManager.java
+++ src/steamHunt/FloorManager.java
@@ -10,7 +10,7 @@ public class FloorManager extends Manager{
     //Get the floor's sprite sheet and make a floor.
     public void initialize(){
         ArrayList<BufferedImage> spriteAnimations = new ArrayList<>();
-        spriteAnimations.add(loadFile("floor.png"));
+        spriteAnimations.add(loadFile("src/resources/foreground/floor/floor.png"));
         getSprites().add(new Floor(0, -3.75f, new Vector2f(14,14), spriteAnimations));
     }
 }
