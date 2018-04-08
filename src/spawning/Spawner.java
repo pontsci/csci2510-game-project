5ab7c53e5e8703d9d6027b020837d5c1aa3627f2
coltondrew <659colton@gmail.com>
2018-04-08 18:19:24 -0500

    New spawning functions.
    Added a render function, started on a test spawn function.

--- src/spawning/Spawner.java
+++ src/spawning/Spawner.java
@@ -1,19 +1,38 @@
 package spawning;
 
-import sprite.*;
 
+import util.Vector2f;
+
+import java.util.Random;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Spawner {
+    // The first spawnRange will always be the player spawn.
     ArrayList<SpawnRange> spawnRanges= new ArrayList<SpawnRange>();
-
+    private Random random;
     public Spawner(){
-
+        random = new Random();
     }
 
     public void setSpawnRanges(ArrayList<SpawnRange> spawnRanges) {
         this.spawnRanges = spawnRanges;
     }
 
+    public Vector2f getSpawnPoint(){
+        return spawnRanges.get(random.nextInt(spawnRanges.size() - 1)+ 1).getSpawnPoint();
+    }
+
+    public Rat spawnRat(){
+        Vector2f spawnpoint = getSpawnPoint();
+    }
+
+
+
+    public void render(Graphics g){
+        for (spawnRange spawn: spawnRanges){
+            spawn.render(g);
+        }
+    }
 
 }
