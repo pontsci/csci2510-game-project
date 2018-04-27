d3739606c9b00854066376b7c107060c117a37ed
Bréana Townsend <cpnhollyshort@gmail.com>
2018-04-27 10:42:39 -0500

    Fixed the screen wall to have the proper assets. Also changed the wall assets to more closely match the platforms.

--- src/sprite/world/ScreenWall.java
+++ src/sprite/world/ScreenWall.java
@@ -5,19 +5,20 @@ import sprite.Sprite;
 import util.Vector2f;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class ScreenWall extends Sprite {
     public ScreenWall(float startX, float startY, Vector2f scale){
         super(startX, startY, scale);
         //Starting Frame
-        setCurrentSpriteFrame(loadFile("src/resources/world/foreground/wall/Wall_WH_85x1590.png"));
+        setCurrentSpriteFrame(loadFile("src/resources/world/foreground/wall/TallWall_WH_314_4200.png"));
         initializeHitboxes();
     }
 
     //Initialize the wall's hitboxes, the first box is the outer hitbox
     public void initializeHitboxes(){
-        hitboxes.add(new BoundingBox(new Vector2f(-5f, -14f), new Vector2f( .45f, 14f),  Color.BLUE));
-        hitboxes.add(new BoundingBox(new Vector2f(-5f, -14f), new Vector2f( .45f, 14f), Color.RED));
+        hitboxes.add(new BoundingBox(new Vector2f(-5f, -24f), new Vector2f( 1.4f, 24f),  Color.BLUE));
+        hitboxes.add(new BoundingBox(new Vector2f(-5f, -24f), new Vector2f( 1.4f, 24f), Color.RED));
     }
 
     @Override
