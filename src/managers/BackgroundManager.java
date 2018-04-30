023f090e3c84f6a2f8138d9a2be41ce6742a40b7
Bréana Townsend <cpnhollyshort@gmail.com>
2018-04-30 11:12:58 -0500

    Efficientized the BG Manager, such that sprites take as little space as possible

--- src/managers/BackgroundManager.java
+++ src/managers/BackgroundManager.java
@@ -11,8 +11,16 @@ import java.awt.image.BufferedImage;
 //Could be used to handle which background to play on each level of the game
 public class BackgroundManager extends Manager{
     private BufferedImage skyboxSpriteSheet = loadFile("src/resources/world/background/skyboxresized.png");
-    private BufferedImage bgWallSpriteSheet = loadFile("src/resources/world/background/Building_WH_400x467_Wall.png");
-
+    private BufferedImage [] bgWallSprites;
+   
+    public BackgroundManager(){
+        super();
+        BufferedImage bgWallSpriteSheet = loadFile("src/resources/world/background/Building_WH_400x467_Wall.png");
+        bgWallSprites = new BufferedImage [9];
+        for(int i=0; i< bgWallSprites.length; i++){
+            bgWallSprites[i] = bgWallSpriteSheet.getSubimage(400*i, 0 , 400, 467);
+        }
+    }        
     public enum BgWallType {
         WINDOWCRACKED(0), WINDOW(1), DIAGONAL(2), MEDIUM (3), BIGHOLE(4), LITTLEHOLE (5), SMALLSEGMENT(6), WALL(7), SCRATCHED(8);  
         private int i;
@@ -36,7 +44,7 @@ public class BackgroundManager extends Manager{
         getSprites().add(new Background(0, 0, new Vector2f(1,1), skyboxSpriteSheet));
         switch(level){
             case 1:
-                 getSprites().add(new BackgroundWall(7, -3, new Vector2f(.5f,.57f), BgWallType.WALL.getIndex(), bgWallSpriteSheet));
+                 getSprites().add(new BackgroundWall(7, -3, new Vector2f(.5f,.57f), bgWallSprites[BgWallType.WALL.getIndex()]));
                 break;
             case 2:
                 
--- src/sprite/world/BackgroundWall.java
+++ src/sprite/world/BackgroundWall.java
@@ -6,6 +6,10 @@ import util.Vector2f;
 
 //If anything special were to happen to the background, such as sliding, it would occur here
 public class BackgroundWall extends Sprite {
+      public BackgroundWall(float startX, float startY, Vector2f scale, BufferedImage spriteSheet){
+        super(startX, startY, scale);
+        setCurrentSpriteFrame(spriteSheet); 
+    }
     public BackgroundWall(float startX, float startY, Vector2f scale, int whichWall, BufferedImage spriteSheet){
         super(startX, startY, scale);
         setCurrentSpriteFrame(spriteSheet.getSubimage(whichWall *400, 0, 400, 467)); 
