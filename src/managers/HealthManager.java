751ec20445b645098c7bffa5c157822068ad1026
Bréana Townsend <cpnhollyshort@gmail.com>
2018-04-30 15:10:56 -0500

    Fixed the HP system.

--- src/managers/HealthManager.java
+++ src/managers/HealthManager.java
@@ -10,10 +10,26 @@ import java.awt.image.BufferedImage;
 
 //This is used for the player right now
 public class HealthManager extends Manager{
-    private BufferedImage healthSpriteSheet = loadFile("src/resources/UI/UIElement_WH_131x203_Battery.png");
+    private BufferedImage [] healthSprites;
     private MainCharacter player;
+    private float cumulativeTime;
+    private int hpShowing;
+    
+    public HealthManager(){
+        super();
+        BufferedImage sheet = loadFile("src/resources/UI/UIElement_WH_131x203_Battery.png");
+        healthSprites = new BufferedImage [3];
+        for(int i =0; i<healthSprites.length;i++){
+            healthSprites[i] = sheet.getSubimage(131*i, 0, 131, 203);
+        }
+    }
+    
     public void initialize(MainCharacter player){
         this.player = player;
+        getSprites().add(new Health(6,4, new Vector2f(.5f, .5f), healthSprites[0]));
+        getSprites().add(new Health(6.5f,4, new Vector2f(.5f, .5f), healthSprites[0]));
+        getSprites().add(new Health(7,4, new Vector2f(.5f, .5f), healthSprites[0]));
+        hpShowing = 3;
     }
 
     @Override
@@ -24,49 +40,77 @@ public class HealthManager extends Manager{
     @Override
     public void process(float delta){
         super.process(delta);
-        switch(player.getHp()){
-            case 1:
-                if(getSprites().size() == 2){
-                    getSprites().remove(1);
+        cumulativeTime += delta;
+        //does the HP bar need to be updated?
+        if (player.getHp() != hpShowing){
+            int empty = getSprites().size() - player.getHp();
+            for(int i=0; i < getSprites().size(); i++){
+                if(empty > i){
+                    getSprites().get(i).setCurrentSpriteFrame(healthSprites[1]);
+                }else{
+                    getSprites().get(i).setCurrentSpriteFrame(healthSprites[0]);
                 }
-                else if(getSprites().size() == 3){
-                    getSprites().remove(2);
-                    getSprites().remove(1);
-                }
-                if(getSprites().size() == 0)
-                    getSprites().add(new Health(5,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(0,0,131,203)));
-                break;
-            case 2:
-                if(getSprites().size() == 3)
-                    getSprites().remove(2);
-                else if(getSprites().size() == 1)
-                    getSprites().add(new Health(6,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(131,0,131, 203)));
-                break;
-            case 3:
-                if(getSprites().size() == 2){
-                    getSprites().add(new Health(5,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(0,0,131,203)));
-                }
-                else if(getSprites().size() == 1){
-                    getSprites().add(new Health(6,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(131,0,131, 203)));
-                    getSprites().add(new Health(5,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(0,0,131,203)));
-                }
-                else if(getSprites().size() == 0){
-                    getSprites().add(new Health(7,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(262,0,131, 203)));
-                    getSprites().add(new Health(6,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(131,0,131, 203)));
-                    getSprites().add(new Health(5,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(0,0,131,203)));
-                }
-                break;
+            }
+            hpShowing = player.getHp();
         }
+        
+        if (hpShowing == 1){
+            //flicker
+            if(cumulativeTime > 1){
+                getSprites().get(2).setCurrentSpriteFrame(healthSprites[2]);
+            }else{
+                getSprites().get(2).setCurrentSpriteFrame(healthSprites[0]);
+            }
+            
+        }
+        if (cumulativeTime > 2){
+            cumulativeTime = 0;
+        }
+        
+//        
+//        switch(player.getHp()){
+//            case 1:
+//                if(getSprites().size() == 2){
+//                    getSprites().remove(1);
+//                }
+//                else if(getSprites().size() == 3){
+//                    getSprites().remove(2);
+//                    getSprites().remove(1);
+//                }
+//                if(getSprites().size() == 0)
+//                    getSprites().add(new Health(5,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(0,0,131,203)));
+//                break;
+//            case 2:
+//                if(getSprites().size() == 3)
+//                    getSprites().remove(2);
+//                else if(getSprites().size() == 1)
+//                    getSprites().add(new Health(6,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(131,0,131, 203)));
+//                break;
+//            case 3:
+//                if(getSprites().size() == 2){
+//                    getSprites().add(new Health(5,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(0,0,131,203)));
+//                }
+//                else if(getSprites().size() == 1){
+//                    getSprites().add(new Health(6,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(131,0,131, 203)));
+//                    getSprites().add(new Health(5,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(0,0,131,203)));
+//                }
+//                else if(getSprites().size() == 0){
+//                    getSprites().add(new Health(7,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(262,0,131, 203)));
+//                    getSprites().add(new Health(6,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(131,0,131, 203)));
+//                    getSprites().add(new Health(5,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(0,0,131,203)));
+//                }
+//                break;
+//        }
     }
 
     @Override
     public void switchLevel(int level, Spawner spawner, Matrix3x3f viewport){
         switch(level){
             case 1:
-                getSprites().clear();
-                getSprites().add(new Health(7,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(262,0,131, 203)));
-                getSprites().add(new Health(6,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(131,0,131, 203)));
-                getSprites().add(new Health(5,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(0,0,131,203)));
+//                getSprites().clear();
+//                getSprites().add(new Health(7,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(262,0,131, 203)));
+//                getSprites().add(new Health(6,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(131,0,131, 203)));
+//                getSprites().add(new Health(5,4, new Vector2f(.5f, .5f), healthSpriteSheet.getSubimage(0,0,131,203)));
                 break;
         }
     }
--- src/sprite/world/Health.java
+++ src/sprite/world/Health.java
@@ -1,5 +1,6 @@
 package sprite.world;
 
+import java.awt.Graphics;
 import sprite.Sprite;
 import util.Vector2f;
 
@@ -7,13 +8,32 @@ import java.awt.image.BufferedImage;
 import java.lang.management.BufferPoolMXBean;
 
 public class Health extends Sprite{
+    private boolean active;
+    
     public Health(float startX, float startY, Vector2f scale, BufferedImage spriteSheet){
         super(startX, startY, scale);
         setCurrentSpriteFrame(spriteSheet);
+        active = true;
     }
 
     @Override
     public void process(float delta){
         //Not needed
     }
+    
+    @Override
+    public void render(Graphics g){
+        if(active){
+            super.render(g);
+        }
+    }
+    
+    public void setActive (boolean change){
+        active = change;
+    }
+    
+    public boolean getActive(){
+        return active;
+    }
+    
 }
