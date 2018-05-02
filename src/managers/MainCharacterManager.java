c06c9313589c687e8f03d0751d1e51b8c29652e3
pontsci <mgfleming@unomaha.edu>
2018-05-01 20:20:04 -0500

    main character manager comments, also changed die method to set health to -1
    Signed-off-by: pontsci <mgfleming@unomaha.edu>

--- src/managers/MainCharacterManager.java
+++ src/managers/MainCharacterManager.java
@@ -10,17 +10,39 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class MainCharacterManager extends Manager{
-    //Get the MainCharacter's sprite sheets and make the main character.
+
+    /**
+     * Get the MainCharacter's sprite sheets and make the main character.
+     * @param floors the floors
+     * @param screenWalls the screen walls
+     * @param powerups the power ups
+     * @param platforms the platforms
+     * @param walls the walls
+     * @param bm the bulletManager
+     * @param doors the doors
+     * @param levers the levers
+     */
     public void initialize(ArrayList<Sprite> floors, ArrayList<Sprite> screenWalls, ArrayList<Sprite> powerups, ArrayList<Sprite> platforms, ArrayList<Sprite> walls, BulletManager bm, ArrayList<Sprite> doors, ArrayList<Sprite> levers){
         getSprites().add(new MainCharacter(0, 0, new Vector2f(-.3f,.3f), floors, screenWalls, powerups, platforms, walls, bm, doors,loadFile("src/resources/character/player/MainCharSprite_WH_237x356_Idle.png"), loadFile("src/resources/character/player/MainCharSprite_WH_237x356_Jump.png"), loadFile("src/resources/character/player/MainCharSprite_WH_237x356_Move.png"), levers));
     }
 
+    /**
+     * Run checkCollision for all MainCharacters
+     * @param delta time
+     * @param viewport the viewport
+     */
     public void checkCollision(float delta, Matrix3x3f viewport){
         for(Sprite sprite : getSprites()){
             ((MainCharacter)sprite).checkCollision(delta, viewport);
         }
     }
 
+    /**
+     * Depending on the level, spawn the MainCharacter in different positions
+     * @param level the current level
+     * @param spawner the spawn system
+     * @param viewport the viewport
+     */
     @Override
     public void switchLevel(int level, Spawner spawner, Matrix3x3f viewport){
         switch(level){
@@ -43,59 +65,86 @@ public class MainCharacterManager extends Manager{
         }
     }
 
-    //Tell all main characters to jump - Note, there should only be one
+    /**
+     * Tell all main characters to jump - Note, there should only be one
+     */
     public void processJump(){
         for(Sprite mainCharacter : getSprites()){
             ((MainCharacter)mainCharacter).jump();
         }
     }
 
-    //Tell all main characters to move left
+    /**
+     * Tell all main characters to move left
+     * @param delta time
+     */
     public void processWalkLeft(float delta){
         for(Sprite mainCharacter : getSprites()){
             ((MainCharacter)mainCharacter).walkLeft(delta);
         }
     }
 
-    //Tell all main characters to move right
+    /**
+     * Tell all main characters to move right
+     * @param delta time
+     */
     public void processWalkRight(float delta){
         for(Sprite mainCharacter : getSprites()){
             ((MainCharacter)mainCharacter).walkRight(delta);
         }
     }
 
-    //Tell all main characters to be idle
+    /**
+     * Tell all main characters to be idle
+     */
     public void processIdle(){
         for(Sprite mainCharacter : getSprites()){
             ((MainCharacter)mainCharacter).idle();
         }
     }
 
-    //Tell all main characters to shoot
+    /**
+     * Tell all main characters to shoot
+     */
     public void processShoot(){
         for(Sprite mainCharacter : getSprites()){
             ((MainCharacter)mainCharacter).shoot();
         }
     }
 
+    /**
+     * Tell all main characters to ignore platforms
+     */
     public void processIgnorePlatformCollision(){
         for(Sprite mainCharacter : getSprites()){
             ((MainCharacter)mainCharacter).ignorePlatformCollision();
         }
     }
 
+    /**
+     * Tell all main characters to not ignore platforms
+     */
     public void processAllowPlatformCollision(){
         for(Sprite mainCharacter : getSprites()){
             ((MainCharacter)mainCharacter).allowPlatformCollision();
         }
     }
 
+    /**
+     * Tell all main characters that they can go through the door
+     */
     public void canGoThroughDoor(){
         for(Sprite player : getSprites()){
             ((MainCharacter)player).canGoThroughDoor();
         }
     }
 
+    /**
+     * Determine if the player is dead
+     * @param xBound x screen bounds
+     * @param yBound y screen bounds
+     * @return whether or not the player is dead
+     */
     public boolean isDead(float xBound, float yBound){
         boolean dead = false;
         for(Sprite mainCharacter : getSprites()){
@@ -109,16 +158,22 @@ public class MainCharacterManager extends Manager{
         }
         return dead;
     }
-    
+
+    /**
+     * reset all main character health to 3
+     */
     public void reset(){
          for(Sprite mainCharacter : getSprites()){
              ((MainCharacter)mainCharacter).setHp(3);
          }
     }
-    
+
+    /**
+     * set all main character health to -1
+     */
     public void die(){
         for(Sprite mainCharacter : getSprites()){
-             ((MainCharacter)mainCharacter).setHp(0);
+             ((MainCharacter)mainCharacter).setHp(-1);
          }
     }
 }
