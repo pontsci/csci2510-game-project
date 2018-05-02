c3884d6b25d33f0b18ee287a79ab461e0f40573e
pontsci <mgfleming@unomaha.edu>
2018-05-01 20:27:27 -0500

    screen manager comments
    Signed-off-by: pontsci <mgfleming@unomaha.edu>

--- src/managers/ScreenManager.java
+++ src/managers/ScreenManager.java
@@ -10,6 +10,9 @@ import util.Matrix3x3f;
 
 //Could be used to handle which background to play on each level of the game
 public class ScreenManager extends Manager{
+    /**
+     * An enum for screen types
+     */
     public enum ScreenType {
         NONE(-1), PAUSE(0), START(1), DIE (2), END(3);
         
@@ -22,9 +25,11 @@ public class ScreenManager extends Manager{
         }
     }
     
-    private ScreenType currentScreen = ScreenType.NONE; 
-    
-    //Get the background's sprite sheet and make a background.
+    private ScreenType currentScreen = ScreenType.NONE;
+
+    /**
+     * Get the background's sprite sheet and make a background.
+     */
     public void initialize(){
         getSprites().add(new DisplayScreen(0, .2f, new Vector2f(1,.87f), loadFile("src/resources/world/background/PauseScreen.png")));//Pause
         getSprites().add(new DisplayScreen(0, 0, new Vector2f(1,.87f), loadFile("src/resources/world/background/StartScreen.png")));//Start
@@ -32,18 +37,26 @@ public class ScreenManager extends Manager{
         getSprites().add(new DisplayScreen(0, 0, new Vector2f(1,.87f), loadFile("src/resources/world/background/EndingScreen.png")));//Ending
         currentScreen = ScreenType.START;
     }
-    
-    // Allow setting the screenType externally
+
+    /**
+     * Allow setting the screenType externally
+     * @param screenType the screen type
+     */
     public void SetScreen(ScreenType screenType) {
         currentScreen = screenType;
     }
-    
+
+    /**
+     * render the current screen
+     * @param g graphics
+     */
     @Override
     public void render(Graphics g){
         if (currentScreen != ScreenType.NONE)
             getSprites().get(currentScreen.getIndex()).render(g);
     }
-      @Override
+    
+    @Override
     public void checkCollision(float delta, Matrix3x3f viewport)
     {
         //no collision needed
